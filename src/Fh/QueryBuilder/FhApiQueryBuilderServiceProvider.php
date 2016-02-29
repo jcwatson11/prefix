b28f323e19fe26b929f16a2c1117d0f086f5b9f7
Jon Watson <jwatson@fh.org>
2016-02-29 12:02:20 -0700

    Corrected configuration file name.

--- src/Fh/QueryBuilder/FhApiQueryBuilderServiceProvider.php
+++ src/Fh/QueryBuilder/FhApiQueryBuilderServiceProvider.php
@@ -6,13 +6,13 @@ class FhApiQueryBuilderServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/config.php' => config_path('fh-api-query-builder.php'),
+            __DIR__.'/config.php' => config_path('fh-laravel-api-query-builder.php'),
         ]);
     }
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/config.php', 'fh-api-query-builder'
+            __DIR__.'/config.php', 'fh-laravel-api-query-builder'
         );
     }
 }
